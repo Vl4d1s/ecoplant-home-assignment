6e44dae1f058e1e9a5428776d47373586dacde8d
Vladis Markin <vladismarkin@gmail.com>
2023-12-22 11:51:47 +0200

    refactor the filters component

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,41 +1,48 @@
 import { Measurement } from "@/types";
-import { Column, Table } from "@tanstack/react-table";
+import { Table, Column } from "@tanstack/react-table";
 
-interface FiltersProps {
+interface FilterProps {
   column: Column<Measurement, unknown>;
   table: Table<Measurement>;
 }
 
-export default function Filters({ column, table }: FiltersProps) {
-  const firstValue = table
+export default function Filter({ column, table }: FilterProps) {
+  const firstRowValue = table
     .getPreFilteredRowModel()
     .flatRows[0]?.getValue(column.id);
+  const currentFilterValue = column.getFilterValue();
 
-  const columnFilterValue = column.getFilterValue();
+  const handleMinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    column.setFilterValue((prevValue: [number, number]) => [
+      Number(e.target.value),
+      prevValue?.[1],
+    ]);
+  };
 
-  return typeof firstValue === "number" ? (
+  const handleMaxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    column.setFilterValue((prevValue: [number, number]) => [
+      prevValue?.[0],
+      Number(e.target.value),
+    ]);
+  };
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    column.setFilterValue(e.target.value);
+  };
+
+  return typeof firstRowValue === "number" ? (
     <div className="flex space-x-2 ">
       <input
         type="number"
-        value={(columnFilterValue as [number, number])?.[0] ?? ""}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [
-            e.target.value,
-            old?.[1],
-          ])
-        }
+        value={(currentFilterValue as [number, number])?.[0] ?? ""}
+        onChange={handleMinChange}
         placeholder={`Min`}
         className="w-24 border pl-1"
       />
       <input
         type="number"
-        value={(columnFilterValue as [number, number])?.[1] ?? ""}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [
-            old?.[0],
-            e.target.value,
-          ])
-        }
+        value={(currentFilterValue as [number, number])?.[1] ?? ""}
+        onChange={handleMaxChange}
         placeholder={`Max`}
         className="w-24 border pl-1"
       />
@@ -44,8 +51,8 @@ export default function Filters({ column, table }: FiltersProps) {
     <div className="flex space-x-2">
       <input
         type="text"
-        value={(columnFilterValue ?? "") as string}
-        onChange={(e) => column.setFilterValue(e.target.value)}
+        value={(currentFilterValue ?? "") as string}
+        onChange={handleTextChange}
         placeholder={`Search...`}
         className="w-36 border pl-1"
       />
